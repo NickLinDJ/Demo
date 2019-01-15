653c71ca36495f0cc7c99521f1dc4ddfeccdb4de
Nick <351925113@qq.com>
2019-01-15 17:54:09 +0800

    测试

--- src/main/java/com/example/demo/controller/TestContrller.java
+++ src/main/java/com/example/demo/controller/TestContrller.java
@@ -1,5 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Nick
+ */
+@RestController
 public class TestContrller {
+
+    @GetMapping("/test")
+    public String Test(String str){
+        return str;
+    }
+
 }
